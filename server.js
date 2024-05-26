f5e38ae3ba0141f0fc6815cc5f3d0c25bf26a714
Vishnusai <vishnusaibore830@gmail.com>
2024-05-26 15:03:45 +0530

    First Relase of backend server

--- server.js
+++ server.js
@@ -4,20 +4,22 @@ const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 const cors = require("cors")
 const bcrypt = require("bcrypt")
+const nodemailer = require('nodemailer')
 const app= express()
 const Port=process.env.PORT
-const saltRounds=process.env.SALT_ROUNDS //For bcrypt hashing
+const saltRounds=10 //For bcrypt hashing
 const jwt = require('jsonwebtoken') // Install 'jsonwebtoken' package
 const secretKey = process.env.SECRET_KEY
-const MongoDB_Atlas_URL= process.env.MONGODB_URL
+const MongoDB_AtlasURL= process.env.MONGODB_URI
 const MongoDB_LocalURL=process.env.MONGODB_LOCAL_URL
 app.use(bodyParser.json())
 app.use(express.json())
 app.use(cors())
+
 //Database Connection
 main().catch(err=>console.log(err))
 async function main(){
-    await mongoose.connect(MongoDB_LocalURL)
+    await mongoose.connect(MongoDB_AtlasURL)
     
     //UserSchema
     const userSchema = new mongoose.Schema({
@@ -27,9 +29,19 @@ async function main(){
         password:String
     })
     const User= mongoose.model("User",userSchema)
+
+    // Configure Nodemailer transporter
+    const App_password = process.env.APP_PASSWORD
+    const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: 'bsai42358@gmail.com', // replace with your email
+      pass: App_password   // replace with your generated App Password
+    }
+    })
     //User Registration
     app.post("/api/register",async(req,res)=>{
-        let fName = req.body.firstNamers
+        let fName = req.body.firstName
         let lName = req.body.lastName
         let Username=req.body.email
         let Password =req.body.password
@@ -45,11 +57,42 @@ async function main(){
                        password:hashedPassword
                     })
                     user.save()
-                    res.json({stat:true,message:"User Registration Success"})
+
+                    // Send registration email
+                    const mailOptions = {
+                        from: 'bsai42358@gmail.com',
+                        to: Username,
+                        subject: 'Anime Blogger Registration Successful 🎉',
+                        html: `
+                          <h2>Welcome To Anime Blogger 🎆</h2>
+                          <h4>Hello ${fName},</h4>
+                          <p>We are absolutely thrilled to have you as part of our community! Thank you for registering on our blog site.</p>
+                          <p>Here's what you can look forward to:</p>
+                    <ul>
+                    <li>📚 Engaging and insightful blog posts</li>
+                    <li>🌟 Exclusive content and updates</li>
+                    <li>🗨️ Community discussions and comments</li>
+                    </ul>
+                          <p>We hope you enjoy your time here and find the content enriching and enjoyable. If you have any questions or need assistance, feel free to reach out to us at any time.</p>
+                        
+                          <p>Thank You 🤩<br>Best regards 🙌<br>Anime Blogger Team 💥</p>
+                          <img src="https://img.freepik.com/free-photo/mythical-dragon-beast-anime-style_23-2151112842.jpg?t=st=1716664493~exp=1716668093~hmac=b34d930aea7f09a69bcb7e9915ea6038c25e075869ee3735cd762e878f1aa7f5&w=500" height="110"alt="App Logo" />
+                        <footer>
+                        <p>If you did not register for this site, please ignore this email.</p>
+                        <p>© 2024 Anime Blogger Hyderabad, Telangana 505526</p> </footer>
+                        `
+                      };
+
+                    transporter.sendMail(mailOptions, (error, info) => {
+                        if (error) {
+                          return console.log(error);
+                        }
+                      });
+                    res.json({stat:true,message:"Welcome To Anime Blogger. Registration Successful"})
                 }
                 else
                 {
-                    res.json({stat:false,message:"Account Alredy Exists"})
+                    res.json({stat:false,message:"Account Alredy Exists!. Please SignIn"})
                 }
             })  
           } catch (error) {
@@ -58,7 +101,7 @@ async function main(){
           }
     })
 
-    //user details fetching
+    //Fetch User Data
     app.get("/api/users",async(req,res)=>{
         try{
             User.find().then(results=>{
@@ -78,7 +121,7 @@ async function main(){
     User.find({email:userName}).then(resul=>{
         if(resul.length===0)
         {
-            res.json({stat:false,message:"User Not Registred"})
+            res.json({stat:false,message:"User Not Registred! Please Sign Up"})
         }
         else{
             User.findOne({email:userName}).then(results=>{
@@ -86,7 +129,7 @@ async function main(){
                     if(isValid){
                         // res.json({stat:true,message:"SuccessFully LoggedIn"})
                         const token = jwt.sign({ userId:userName}, secretKey);
-                        res.json({token,stat:true,message:"SuccessFully Logged In"})
+                        res.json({token,User:userName,stat:true,message:"SuccessFully Logged In"})
                     }
                     else{
                         res.json({stat:false,message:"Incorrect Password"})
@@ -104,10 +147,36 @@ async function main(){
         let mailId = req.body.email
         let newPassword = req.body.password
         let newHashedPassword = await bcrypt.hash(newPassword,saltRounds)
+
+        //Sending Password Reset Mail
+        const mailOptions = {
+            from: 'bsai42358@gmail.com',
+            to: mailId,
+            subject: 'Anime Blogger!Password Reset Successful 🎉',
+            html: `
+              <h4>Hello ${mailId}</h4>
+              <p> Your Anime Blogger Account Password Changed SuccessFully ☑ <br>
+              SignIn to Your Account With New Credentials </p>
+              <p>Hope You doing Well 🤩</p>
+              <p>Thank You 😊<br>Best regards 🙌<br>Anime Blogger Team 💥</p>
+              <img src="https://img.freepik.com/free-photo/mythical-dragon-beast-anime-style_23-2151112842.jpg?t=st=1716664493~exp=1716668093~hmac=b34d930aea7f09a69bcb7e9915ea6038c25e075869ee3735cd762e878f1aa7f5&w=500" height="110"alt="App Logo" />
+              <footer>
+              <h5>‼ If this was you, you don’t need to do anything. If not, we’ll help you secure your account.</h5>
+              <p>© 2024 Anime Blogger Hyderabad, Telangana 505526</p> </footer>
+            `
+          };
+        //
+        
         try{
         User.findOneAndUpdate({email:mailId},{password:newHashedPassword}).then(isUpdated=>{
             let mail=isUpdated.email
+            transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+            return console.log(error);
+            }
             res.json({username:mail,stat:true,message:" Your Password Changed Successfully"})
+            });
+
         }).catch(err=>console(err))
     }catch(err){
         console.log(err)
@@ -126,13 +195,49 @@ async function main(){
     //POST Route -->Blog Posts
     app.post("/api/posts",async(req,res)=>{
         const{name,blogImage,content}=req.body
+        const LoggedUser = req.headers['header-1']
+
+        // Mail Options for Congratulations Message
+        const mailOptions = {
+            from: 'bsai42358@gmail.com',
+            to: LoggedUser,
+            subject: 'Anime Blogger!Congratulations on Your New Post! 🌟',
+            html: `
+              <h2>Congratulations on Your New Post! 🎆</h2>
+              <h4>Hello ${LoggedUser},</h4>
+              <p>We are thrilled to see your new post on Anime Blogger.Your contributions are what make our community vibrant and engaging.</p>
+              <p>Here are a few things you can do to maximize the impact of your post:</p>
+            <ul>
+                <li>📣 Share it on social media to reach a wider audience</li>
+                <li>💬 Engage with readers by responding to comments</li>
+                <li>🔗 Link to related posts to provide more value</li>
+                <li>🌟 Update your post with new information as it becomes available</li>
+            </ul>
+            <p>Thank you for your valuable contribution! We look forward to seeing more of your insightful posts and engaging content.</p>
+            <p>If you have any questions or need assistance, feel free to reach out to us at any time.</p>
+            <p>Happy blogging 🥳</p>
+            <p>Best regards 🙌<br>Anime Blogger Team 💥</p>
+            <img src="https://img.freepik.com/free-photo/mythical-dragon-beast-anime-style_23-2151112842.jpg?t=st=1716664493~exp=1716668093~hmac=b34d930aea7f09a69bcb7e9915ea6038c25e075869ee3735cd762e878f1aa7f5&w=500" height="110"alt="App Logo" />
+            <footer>
+            <p>If you did not write this post, please contact us immediately.</p>
+            <p>© 2024 Anime Blogger Hyderabad, Telangana 505526</p> </footer>
+            `
+          };
+
+        //
         const post=new Post({
             name,blogImage,content
         })
-
         try{
             await post.save()
-            res.json({stat:true,message:"Blog Published SuccessFully"})
+            //Sending Mail
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                return console.log(error);
+                }
+                res.json({stat:true,message:"Blog Published SuccessFully"})
+                });
+
         }catch(err){
             console.error(err)
             res.status(500).json({message:"server Error"})
@@ -151,7 +256,7 @@ async function main(){
         }
     })
     
-    //getting a single post
+    //GET--> single post
     app.get("/api/post/:id",async(req,res)=>{
         try{
             Post.findById(req.params.id).then(result=>{
@@ -162,9 +267,45 @@ async function main(){
             res.status(500).json({message:"Server Error"})
         }
     })
+    
+    //Patch Route -->BlogPosts
+    app.patch("/api/edit-post/:id",async(req,res)=>{
+        let Pid=req.params.id
+        let PName=req.body.name
+        let imgURL=req.body.blogImage
+        let PContent=req.body.content
+        try{
+            if((PName==="")&&(imgURL==="")){
+                Post.findOneAndUpdate({_id:Pid},{content:PContent}).then(r=>{
+                    res.json({message:"Blog Content Updated"})
+                }).catch(err=>console(err))
+
+            }else if((PName==="")&&(PContent===""))
+                {
+                Post.findOneAndUpdate({_id:Pid},{blogImage:imgURL}).then(re=>{
+                    res.json({message:"Blog Image Updated"})
+                }).catch(err=>console(err))
+
+            }else if((imgURL==="")&&(PContent==="")){
+                Post.findOneAndUpdate({_id:Pid},{name:PName}).then(re=>{
+                    res.json({message:"Blog Post Name Updated"})
+                }).catch(err=>console(err))
+            }
+            else{
+                Post.findOneAndUpdate({_id:Pid},{name:PName,blogImage:imgURL,content:PContent}).then(reslt=>{
+                    res.json({message:"Blog Updated SuccessFully"})
+                    }
+                ).catch(err=>console(err))
+            }
+        }catch(err){
+            console.error(err)
+            res.status(500).json({message:"Server Error"})
+        }
+    })
+
 
     //Delete Route  -->Blog Posts
-    app.delete("/api/posts/:id",async(req,res)=>{
+    app.delete("/api/delete-post/:id",async(req,res)=>{
         try{
             await Post.findByIdAndDelete(req.params.id)
             res.json({stat:true,message:"Blog Deleted Successfully"})
